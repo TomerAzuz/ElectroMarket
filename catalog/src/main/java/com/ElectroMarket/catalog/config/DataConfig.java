218a6373c414cf0654814a3a9228bdc29da095fc
TomerAzuz <tomerazuz1@gmail.com>
2023-09-13 23:50:12 +0300

    test2

--- catalog/src/main/java/com/ElectroMarket/catalog/config/DataConfig.java
+++ catalog/src/main/java/com/ElectroMarket/catalog/config/DataConfig.java
@@ -3,7 +3,6 @@ package com.ElectroMarket.catalog.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
-//test
 @Configuration
 @EnableJdbcAuditing
 public class DataConfig {}
