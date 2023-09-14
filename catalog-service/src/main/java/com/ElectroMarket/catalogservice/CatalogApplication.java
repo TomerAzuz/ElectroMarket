935dd6490c7826f836558b6e42848f2be795eb11
TomerAzuz <tomerazuz1@gmail.com>
2023-09-14 22:45:26 +0300

    postgres and catalog service deployment

--- catalog-service/src/main/java/com/ElectroMarket/catalogservice/CatalogApplication.java
+++ catalog-service/src/main/java/com/ElectroMarket/catalogservice/CatalogApplication.java
@@ -2,8 +2,10 @@ package com.ElectroMarket.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogApplication {
 
 	public static void main(String[] args) {
