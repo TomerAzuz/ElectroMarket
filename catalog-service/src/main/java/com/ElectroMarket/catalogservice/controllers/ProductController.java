de83f157fd1506fda0fb71f4dc98814f1c3811fe
TomerAzuz <tomerazuz1@gmail.com>
2023-09-17 16:11:59 +0300

    test

--- catalog-service/src/main/java/com/ElectroMarket/catalogservice/controllers/ProductController.java
+++ catalog-service/src/main/java/com/ElectroMarket/catalogservice/controllers/ProductController.java
@@ -5,7 +5,7 @@ import com.ElectroMarket.catalogservice.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+//
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
